e6c200f1b18821324a6d43177bf0c124923ae56b
vasyaod <vasyaod@difur.ru>
2012-11-05 14:48:35 +0600

    Добавлены картинки и README.
    Signed-off-by: vasyaod <vasyaod@difur.ru>

--- web/data.php
+++ web/data.php
@@ -2,11 +2,21 @@
 	if ($_REQUEST["action"] == "save") {
 		$fileName = $_REQUEST["file"] == null ? "main.json" : $_REQUEST["file"];
 		$postdata = file_get_contents("php://input");		// $HTTP_RAW_POST_DATA
-		file_put_contents("schemas/".$fileName, $postdata);
-		echo "данные успешно сохранены.";
+
+		if (basename($fileName) == $fileName) {
+			file_put_contents("schemas/".$fileName, $postdata);
+			echo "данные успешно сохранены.";
+		} else {
+			echo "ошибка имени файла";
+		}
+
 	} else if ($_REQUEST["action"] == "load") {
 		$fileName = $_REQUEST["file"] == null ? "main.json" : $_REQUEST["file"];
-		echo file_get_contents("schemas/".$fileName);
+		if (basename($fileName) == $fileName) {
+			echo file_get_contents("schemas/".$fileName);
+		} else {
+			echo "";
+		}
 	} else {
 		echo "ошибка, неизвестный параметр.";
 	}
